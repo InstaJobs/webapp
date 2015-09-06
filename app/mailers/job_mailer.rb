4ca6db6dfe6f7b502681bc066544b12800ad9127
Asankhaya Sharma <asankhaya@yahoo.com>
2015-09-06 12:19:26 +0800

    Fix broken image links in mail notificaitons
    This fixes #4

--- app/mailers/job_mailer.rb
+++ app/mailers/job_mailer.rb
@@ -3,8 +3,8 @@ class JobMailer < ApplicationMailer
 	def saved(email, job, user)
 		@job = job
 		@user = user
-		@propic = @user.avatar.url || "http://intajobs.com/assets/images/propic.jpg"
-		@thumb = @user.avatar.thumb.url || "http://intajobs.com/assets/images/propic_thumb.jpg"
+		@propic = @user.avatar.url || "http://instajobs.io/assets/images/propic.jpg"
+		@thumb = @user.avatar.thumb.url || "http://instajobs.io/assets/images/propic_thumb.jpg"
 		domain = ENV["DOMAIN"] || "instajobs.io"; 
 		@link = domain+"/dashboard/candidates/" + @job.id.to_s; 
 		mail(to: email, subject: "A candidate is interested in your job posting")
@@ -12,7 +12,7 @@ class JobMailer < ApplicationMailer
 	def match(candidate, job)
 		@candidate = candidate
 		@job = job
-		@thumb = @candidate.avatar.thumb.url || "http://intajobs.com/assets/images/propic_thumb.jpg"
+		@thumb = @candidate.avatar.thumb.url || "http://instajobs.io/assets/images/propic_thumb.jpg"
 		domain = ENV["DOMAIN"] || "instajobs.io"; 
 		@link = domain+"/dashboard/matches/"; 
 		mail(to: @candidate.email, subject: "Congrats, you have been matched with a job!");
